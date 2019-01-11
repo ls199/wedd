34a1fc55f762fe74d748ca17e297f8e5a36b1bde
baozhenqin <1078386131@qq.com>
2019-01-11 13:23:57 +0800

    dddd

---
+++ Music/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string SongName { get; set; }
+        public string NewPath { get; set; }
+    public string EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
